37e58fb2bed8cbbc5ad6c782f41180bff6af5b6a
Yuuuno <yuuuno32@gmail.com>
2022-04-28 03:29:55 -0700

    algo expert

---
+++ algoexpert/validate subsequence.py
@@ -0,0 +1,19 @@
+# find subsequence
+def validateSubsequence(array,sequence):
+    arrIdx=0
+    seqIdx=0
+    while arrIdx<len(array) and seqIdx<len(sequence):
+        if array[arrIdx]==sequence[seqIdx]:
+            seqIdx+=1
+        arrIdx+=1
+    return seqIdx==len(sequence)
+
+def validateSubsequence(array,sequence):
+    seqIdx=0
+    for value in array:
+        if seqIdx==len(sequence):
+            break
+        if sequence[seqIdx]==value:
+            seqIdx+=1
+    return seqIdx==len(sequence)
+
