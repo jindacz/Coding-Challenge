92d2e475d3370e2da62b61b09844fb44da043041
Yuuuno <yuuuno32@gmail.com>
2022-06-20 03:03:40 -0700

    ll XXIV

--- leetcode/linked list/206. Reverse Linked List ::5.3.py
+++ leetcode/linked list/206. Reverse Linked List ::5.3.py
@@ -1,31 +1,31 @@
-206. Reverse Linked List
-Given the head of a singly linked list, reverse the list, and return the reversed list.
+# 206. Reverse Linked List
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
 
  
 
-Example 1:
+# Example 1:
 
 
-Input: head = [1,2,3,4,5]
-Output: [5,4,3,2,1]
-Example 2:
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+# Example 2:
 
 
-Input: head = [1,2]
-Output: [2,1]
-Example 3:
+# Input: head = [1,2]
+# Output: [2,1]
+# Example 3:
 
-Input: head = []
-Output: []
+# Input: head = []
+# Output: []
  
 
-Constraints:
+# Constraints:
 
-The number of nodes in the list is the range [0, 5000].
--5000 <= Node.val <= 5000
+# The number of nodes in the list is the range [0, 5000].
+# -5000 <= Node.val <= 5000
  
 
-Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+# Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
 
 
 
@@ -38,4 +38,21 @@ Follow up: A linked list can be reversed either iteratively or recursively. Coul
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    	curr
+    	left=right=head #初始化
+
+    	if head==None: #边界条件none
+    		return None
+
+    	if right.next==None: #边界条件：只有一个元素
+    		return head
+
+    	else:
+    		right=right.next #right先行
+    		left.next=None #原来的第一个变成现在的最后一个
+
+    	while right!=None: #开始循环
+    		head=right #head第二个行动
+    		right=right.next #right再往右一步
+    		head.next=left #箭头转向
+    		left=head #left往右边
+    	return head
---
+++ leetcode/linked list/24. swap nodes.py
@@ -0,0 +1,32 @@
+# Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        dummy = ListNode(0) 
+        dummy.next = head # set dummyHead
+        temp = dummy
+
+        while temp.next and temp.next.next: # [du, 1, 2, 3] -> [du, 2, 1, 3]
+            node1 = temp.next # init/update mid ptr
+            node2 = temp.next.next # init/update right ptr
+            
+            temp.next = node2 # du -> 2
+            node1.next = node2.next # 1 -> 3
+            node2.next = node1  # 2 -> 1
+            temp = node1 # temp -> 3
+        return dummy.next
+
+
+# t- o(n)
+# s- o(1)
+        
