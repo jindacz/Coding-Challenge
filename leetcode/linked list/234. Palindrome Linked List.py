52bc6c4e7f3c6622428ffa9b28f3d1c3953fe3d8
Yuuuno <yuuuno32@gmail.com>
2022-06-22 02:57:00 -0700

    lc ll problem

--- leetcode/linked list/234. Palindrome Linked List.py
+++ leetcode/linked list/234. Palindrome Linked List.py
@@ -1,9 +1,22 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# 1 reach the end of linkedlist by recursion
+# 2 compare if the last node is same as the first nodes
+# 3 second previous node has the same value as the second node
+# 4 use stack and pointer
+def isPalindrome(self, head):
+
+
+
+
+
+# o(n) space
+
     def isPalindrome(self, head):
         """
         :type head: ListNode
---
+++ leetcode/linked list/82. Remove Duplicates from Sorted List II.py
@@ -0,0 +1,34 @@
+# Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        dummy = ListNode(-1)
+        dummy.next = head
+        
+        pre = dummy
+        cur = head
+        
+        while cur:
+            # 跳过当前的重复节点，使得cur指向当前重复元素的最后一个位置
+            while cur.next and cur.val == cur.next.val:
+                cur = cur.next
+            if pre.next == cur:
+                # pre和cur之间没有重复节点，pre后移
+                pre = pre.next
+            else:
+                # pre->next指向cur的下一个位置（相当于跳过了当前的重复元素）
+                # 但是pre不移动，仍然指向已经遍历的链表结尾
+                pre.next = cur.next
+            
+            cur = cur.next
+        return dummy.next
