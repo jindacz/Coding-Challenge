3451fe176177ceedefb74678f21a283d59e73b2e
Yuuuno <yuuuno32@gmail.com>
2022-07-16 02:06:23 -0700

    lc53

--- leetcode/Array/1. Two Sum ::4.9.py
+++ leetcode/Array/1. Two Sum ::4.9.py
@@ -1,93 +1,15 @@
-1. Two Sum
-Easy
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-27873
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-894
+# You can return the answer in any order.
 
-Add to List
-
-Share
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
- 
-
-Example 1:
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
-
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
- 
-
-Constraints:
-
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
- 
-
-Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
-
-
-4.9 两数之后
-4.9.1 算法要求
-给定一个整数数组nums和一个目标target，请在该数组中找出和为目标值的那两个整数，并返回它们的数组下标。
-你可以假设每种输入只会对应一个答案，但是你不能重复利用这个数组中相同的元素。
-
-示例：
-nums=【2，7，11，15】，target=9
-
-因为nums0+nums1=2+8=9
-所以返回【0，1】
-
-4.9.2解题思路
-1.两数和常规算法
-求两数之后等于目标值，常规的做法是嵌套循环nums列表。用两个for循环，分组取出两个数字之后。然后和target做比较，如果相等，就返回两个数的下标。复杂度o-n平方。nums=【2，7，11，15】，target=9为例。
-好处简单明了，坏处是比较费时。
-
-2.两数和简化算法
-两数之后和等于target，可以列为numsi+numsj=target。转换一下就可以得到numsi=target-numsj。现在只需要遍历一次nums，确定两数差是否在子列表就可以了。遍历nums【i】，然后定位target-numsi的位置，也就是下标j的值。
-这样两次循环简化成了一次。
-
-4.9.3
-1.常规
-
+# 2.简化版
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         for i in range(len(nums)-1):
-        	for j in range(i+1,len(nums)):
-        		if nums[i]+nums[j]==target:
-        			return [i,j]
+            if target-nums[i] in nums[i+1:]:
+                j = nums[i+1:].index(target-nums[i])
+            return [i, i+j+1]  # return relative position of j
 
-2.简化版
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)-1):
-        	if target-nums[i] in nums[i+1:]: #利用python的in运算，来获取numsj的值
-        		j=nums[i+1:].index(target-nums[i]) #利用index函数获取差值在nums[i+1:]中的相对序号j
-        	return [i,i+j+1] #返回时通过计算j在列表的绝对序号i+j+1
-
-two sum简化算法先利用python的in运算，来获取numsj的值，然后利用index函数获取差值在nums[i+1:]中的相对序号j，返回时通过计算j在列表的绝对序号i+j+1.相比常规算法，o-n节约时间。
+# two sum简化算法先利用python的in运算，来获取numsj的值，然后利用index函数获取差值在nums[i+1:]中的相对序号j，返回时通#过计算j在列表的绝对序号i+j+1.相比常规算法，o-n节约时间。
--- leetcode/dp/53. Maximum Subarray.py
+++ leetcode/dp/53. Maximum Subarray.py
@@ -12,20 +12,19 @@
 # 如果实现了o-n，尝试用分而治之
 
 # 9.3.2
-# 这道题动态规划很简单。就是假设列表中第一个数为最大子胥河，然后逐个将列表后的元素叠加到最大子序和中。如果最大子序和变大了，就修改最大子序和的value，继续往下叠加。如果变小了就放弃最后叠加的数字（必定是负数）。从最后叠加的那个数（负数）之后，重新开始叠加计算最大子序和。最后得到的就是整个列表的最大子序和。以nums=【-2，1，-3，4，-1，2，1，-5，4】为例。
+# 这道题动态规划很简单。就是假设列表中第一个数为最大subsum，然后逐个将列表后的元素叠加到最大子序和中。如果最大子序和变大了，就修改最大子序和的value，继续往下叠加。如果变小了就放弃最后叠加的数字（必定是负数）。从最后叠加的那个数（负数）之后，重新开始叠加计算最大子序和。最后得到的就是整个列表的最大子序和。以nums=【-2，1，-3，4，-1，2，1，-5，4】为例。
 
 # 用较大的subsum替代掉maxsun，一直叠加到列表完毕，最后得到的maxsum就是最大子序和。
 
-9.3.3
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        subCount=0
-        maxCount=nums[0] #先假设最大子序和就是列表第一个元素
+        subCount = 0
+        maxCount = nums[0]  # 先假设最大子序和就是列表第一个元素
 
-        for i in range(len(nums)): #for i=[0,...,len(nums)]
-        	subCount+=nums[i]
-        	if subCount>maxCount:
-        		maxCount=subCount  #计算子序和，如果将子序和比假设的最大子序和大，就替代假设的最大子序和
-        	if subCount<0: #如果subcount比0小，就放弃这次统计
-        		subCount=0
+        for i in range(len(nums)):  # for i=[0,...,len(nums)]
+            subCount += nums[i]
+            if subCount > maxCount:
+                maxCount = subCount  # 计算子序和，如果将子序和比假设的最大子序和大，就替代假设的最大子序和
+            if subCount < 0:  # 如果subcount比0小，就放弃这次统计
+                subCount = 0
         return maxCount
